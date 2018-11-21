9b743c30983785beedbafaf9066fa55c4692fb19
Daniel Boyd <dan_boyd20@hotmail.com>
2018-11-21 13:11:50 +0000

    Added snake mechanics

---
+++ BattleSnake/Assets/Scripts/SnakeController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnakeController : MonoBehaviour {
+
+    float timer = 0.0f;
+    Vector3 moveDirection = Vector3.right;
+    Vector3 currentMoveDirection = Vector3.right;
+    SnakePiece snakePiece;
+
+    GameObject[] collectables;
+
+    // Use this for initialization
+    void Start () {
+        snakePiece = GetComponent<SnakePiece>();
+        collectables = GameObject.FindGameObjectsWithTag("Collectable");
+        snakePiece.AddPiece();
+    }
+	//4096 / 512
+	// Update is called once per frame
+	void Update () {
+
+        if(Input.GetKeyDown(KeyCode.RightArrow) && currentMoveDirection != Vector3.left)
+        {
+            moveDirection = Vector3.right;
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow) && currentMoveDirection != Vector3.right)
+        {
+            moveDirection = Vector3.left;
+        }
+        if (Input.GetKeyDown(KeyCode.UpArrow) && currentMoveDirection != Vector3.down)
+        {
+            moveDirection = Vector3.up;
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow) && currentMoveDirection != Vector3.up)
+        {
+            moveDirection = Vector3.down;
+        }
+
+        timer += Time.deltaTime;
+        if(timer > 0.5f)
+        {
+            snakePiece.MovePiece(moveDirection);
+            currentMoveDirection = moveDirection;
+            timer -= 1.0f;
+            for (int i = 0; i < collectables.Length; ++i)
+            {
+                if(collectables[i].transform.position == transform.position)
+                {
+                    Debug.Log("Collected");
+                    transform.parent.GetChild(transform.parent.childCount - 1).GetComponent<SnakePiece>().AddPiece();
+                }
+            }
+        }
+    }
+}
---
+++ BattleSnake/Assets/Scripts/SnakePiece.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnakePiece : MonoBehaviour {
+
+    Vector3 currentDirection = Vector3.right;
+    Vector3 previousDirection = Vector3.right;
+    SnakePiece nextPiece = null;
+
+    private void setNextPiece()
+    {
+        if (transform.parent.childCount > transform.GetSiblingIndex() + 1)
+        {
+            Transform next = transform.parent.GetChild(transform.GetSiblingIndex() + 1);
+            if (next)
+            {
+                nextPiece = next.GetComponent<SnakePiece>();
+            }
+        }
+    }
+
+    public void AddPiece()
+    {
+        Vector3 newPosition = transform.position;
+        if(currentDirection == Vector3.right)
+        {
+            newPosition.x -= 1.0f;
+        }
+        if (currentDirection == Vector3.left)
+        {
+            newPosition.x += 1.0f;
+        }
+        if (currentDirection == Vector3.up)
+        {
+            newPosition.y -= 1.0f;
+        }
+        if (currentDirection == Vector3.down)
+        {
+            newPosition.y += 1.0f;
+        }
+        Instantiate(Resources.Load("Piece") as GameObject, newPosition, transform.rotation, transform.parent);
+        setNextPiece();
+    }
+
+    // Use this for initialization
+    void Start () {
+        setNextPiece();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void MovePiece(Vector3 direction)
+    {
+        previousDirection = currentDirection;
+        currentDirection = direction;
+        transform.Translate(direction);
+        if(nextPiece)
+        {
+            nextPiece.MovePiece(previousDirection);
+        }
+    }
+}
